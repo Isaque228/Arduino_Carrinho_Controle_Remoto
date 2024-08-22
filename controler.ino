52c2aeda3a2c2b552b6563021d10d17a71e399f7
IsaquePereira <120292898+Isaque228@users.noreply.github.com>
2024-08-22 18:30:24 -0300

    codigo do controler

---
+++ controler.ino
@@ -0,0 +1,23 @@
+//**********************************************************************************
+/*
+Codigo para receber o codigo do controle.
+Versão depreciada refazer com a versão mais recente
+Existe limitações como o tempo de comando e não são continuos
+*/
+#include <IRremote.h>
+int RECV_PIN = 11;
+IRrecv irrecv(RECV_PIN);
+decode_results results;
+void setup()
+{
+  Serial.begin(9600);
+  irrecv.enableIRIn();
+}
+void loop() {
+  if (irrecv.decode(&results)) {
+    Serial.println(results.value, HEX);
+    irrecv.resume();
+  }
+  delay(100);
+}
+//**********************************************************************************
