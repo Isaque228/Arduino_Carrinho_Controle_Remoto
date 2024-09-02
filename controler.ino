a55276d408b127ac0fc8447b4ddae6cf6add67c3
Lucas Vieira <lucasvieira291003@gmail.com>
2024-09-02 00:31:56 -0300

    Update controler.ino

--- controler.ino
+++ controler.ino
@@ -5,14 +5,16 @@ Versão depreciada refazer com a versão mais recente
 Existe limitações como o tempo de comando e não são continuos
 */
 #include <IRremote.h>
+
 int RECV_PIN = 11;
 IRrecv irrecv(RECV_PIN);
 decode_results results;
-void setup()
-{
+
+void setup() {
   Serial.begin(9600);
   irrecv.enableIRIn();
 }
+
 void loop() {
   if (irrecv.decode(&results)) {
     Serial.println(results.value, HEX);
